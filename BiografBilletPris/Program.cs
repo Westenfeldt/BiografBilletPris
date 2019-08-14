65b24728e81904afe2969d99a5d444f50fdce544
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-14 14:12:18 +0200

    Add project files.

---
+++ BiografBilletPris/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BiografBilletPris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string alder;
+            double age;
+            double billet18 = 35;
+            double billet1860 = 60;
+            double billet60 = 30;
+
+            Console.WriteLine("Velkommen til online delen af vores billetsystem. Angiv alder for at se priser");
+
+            alder = Console.ReadLine();
+
+            age = float.Parse(alder);
+
+            if (age < 18)
+            {
+                Console.WriteLine("Din billet pris er {0}", billet18);
+            }
+            else if (age >= 18 && age < 60)
+            {
+                Console.WriteLine("Din billet pris er {0}", billet1860);
+            }
+            else if (age > 60)
+            {
+                Console.WriteLine("Din billet pris er {0}", billet60);
+            }
+
+
+
+
+
+        }
+    }
+}
